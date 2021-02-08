038ae67145063f8636fbdda4b6d5c1cf9ba7fa4b
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-02-07 21:26:18 -0800

    removed unnecessary imports

--- tests/open_mp_tests/cython_test.py
+++ tests/open_mp_tests/cython_test.py
@@ -4,11 +4,10 @@ from pymaxion.constraints.cable import Cable
 from pymaxion.constraints.anchor import Anchor
 from pymaxion.constraints.force import Force
 import json
-import time
 
 if __name__ == "__main__":
     fp = './ParticleSystem.json'
     psystem = ParticleSystem.from_json(fp)
-    psystem.solve(ke=1e-10, max_iter=10000, parallel=True)
+    psystem.solve(ke=1e-10, max_iter=10000)
     print(psystem.num_iter)
 
