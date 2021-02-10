d80ad142fe14c385b6f1c441c061199c353d5016
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-02-09 20:39:41 -0800

    fixed relative path issue with test

--- tests/open_mp_tests/cython_test.py
+++ tests/open_mp_tests/cython_test.py
@@ -4,10 +4,15 @@ from pymaxion.constraints.cable import Cable
 from pymaxion.constraints.anchor import Anchor
 from pymaxion.constraints.force import Force
 import json
+import os
 
-if __name__ == "__main__":
-    fp = './ParticleSystem.json'
+BASE_FOLDER = os.path.dirname(__file__)
+
+def run_system():
+    fp = os.path.join(BASE_FOLDER, 'ParticleSystem.json')
     psystem = ParticleSystem.from_json(fp)
     psystem.solve(ke=1e-10, max_iter=10000)
     print(psystem.num_iter)
 
+if __name__ == "__main__":
+    run_system()
