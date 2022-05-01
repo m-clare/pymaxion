9db6316495b105a0c4e8576a101a77279652741c
Maryanne Wachter <maryanne.wachter@gmail.com>
2022-05-01 08:45:45 -0600

    fixed blender version in init file

--- src/addons/blenderPymaxion/__init__.py
+++ src/addons/blenderPymaxion/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Pymaxion",
     "description": "Projection based physics simulator/geometric constraint solver",
     "author": "Maryanne Wachter",
-    "blender": (2, 91, 0),
+    "blender": (3, 1, 0),
     "version": (0, 0, 1),
     "location": "Scene / Object / Mesh Properties",
     "category": "Object"
@@ -13,4 +13,3 @@ from .blender_pymaxion import *
 
 if __name__ == "__main__":
     register()
-
